4c046d839e55adab46cb7c4ac73eee1a06b702dd
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-16 00:01:35 +0530

    Create Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import {BrowserRouter , Route , Link} from 'react-router-dom';
+import StreamCreate from './components/streams/StreamCreate';
+import StreamDelete from './components/streams/StreamDelete';
+import StreamEdit from './components/streams/StreamEdit';
+import StreamList from './components/streams/StreamList';
+import StreamShow from './components/streams/StreamShow';
 
-
-function App() {
+const App = ()=> {
   return (
     <div >
-          app
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={StreamList}/>
+            <Route path="/streams/new" exact component={StreamCreate}/>
+            <Route path="/streams/edit" exact component={StreamEdit}/>
+            <Route path="/streams/delete" exact component={StreamDelete}/>
+            <Route path="/streams/show" exact component={StreamShow}/>
+          </div>
+        </BrowserRouter>
     </div>
   );
 }
