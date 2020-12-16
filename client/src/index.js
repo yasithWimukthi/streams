55823fd2fec6bffebe6076775b3315e0f6f74803
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-16 23:18:22 +0530

    Install redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
