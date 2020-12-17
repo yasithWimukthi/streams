b2767b7164596ce49aa8f7f639b634a66303e9b2
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-17 18:16:20 +0530

    Implement authReducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,16 @@
+const INITIAL_STATE = {
+    isSignedIn: null
+}
+
+export default (state = INITIAL_STATE , action) =>{
+
+    switch(action.type){
+
+        case 'SIGN_IN' :
+            return {...state, isSignedIn : true};
+        case 'SIGN_OUT' :
+            return {...state, isSignedOut : false};
+        default :
+            return state;
+    }
+};
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux';
+import authReducer from './authReducer';
 
 export default combineReducers({
-    dummy: () => 5
+    auth : authReducer
 })
