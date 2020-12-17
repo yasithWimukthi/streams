8c2e7e77759f1268717cce8e72231fdbf41232c5
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-17 18:43:51 +0530

    Edit authReducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import {SIGN_IN,SIGN_OUT} from '../actions/types'
+
 const INITIAL_STATE = {
     isSignedIn: null
 }
@@ -6,9 +8,9 @@ export default (state = INITIAL_STATE , action) =>{
 
     switch(action.type){
 
-        case 'SIGN_IN' :
+        case SIGN_IN :
             return {...state, isSignedIn : true};
-        case 'SIGN_OUT' :
+        case SIGN_OUT :
             return {...state, isSignedOut : false};
         default :
             return state;
