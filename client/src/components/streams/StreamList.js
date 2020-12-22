7fbd5f2cf6f5dd30f7d51bd7d77105111b36d790
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-22 15:58:28 +0530

    Edit StreamList component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -12,7 +12,7 @@ class StreamList extends Component {
     renderAdmin(stream){
         return (
             <div className="right floated content">
-                <button className="ui button primary">Edit</button>
+                <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
                 <button className="ui button negative">Delete</button>
             </div>
         )
